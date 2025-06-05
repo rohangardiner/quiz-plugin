cbbb148a7cb38732ac5b40908eff106ca298ff5b
Rohan <71481034+rohangardiner@users.noreply.github.com>
2025-06-06 09:56:21 +1000

    Updated class naming for Github updater

--- includes/class-career-quiz-updater.php
+++ includes/class-career-quiz-updater.php
@@ -1,7 +1,7 @@
 <?php
 
 // Prevent loading this file directly and/or if the class is already defined
-if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdater' ) || class_exists( 'WP_GitHub_Updater' ) )
+if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdaterCareerQuiz' ) || class_exists( 'WP_GitHub_Updater_Career_Quiz' ) )
 	return;
 
 /**
